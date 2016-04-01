9fa001191fa8551bc25761ea17dbb9fd17b7807f
aash29 <aash29@gmail.com>
2016-04-01 16:36:14 +0300

    bullets

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 /*  Copyright 2012-2016 Sven "underscorediscovery" Bergström
-    
+
     written by : http://underscorediscovery.ca
     written for : http://buildnewgames.com/real-time-multiplayer/
-    
+
     MIT Licensed.
 */
 
@@ -18,13 +18,38 @@ window.onload = function(){
 
 			//Fetch the viewport
 		game.viewport = document.getElementById('viewport');
-			
+
 			//Adjust their size
 		game.viewport.width = game.world.width;
 		game.viewport.height = game.world.height;
 
 			//Fetch the rendering contexts
 		game.ctx = game.viewport.getContext('2d');
+
+
+    function getMousePos(canvas, evt) {
+      var rect = canvas.getBoundingClientRect();
+      return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+     }
+    };
+
+      game.viewport.addEventListener('mousemove', function(evt) {
+      var mousePos = getMousePos(game.viewport, evt);
+      //var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+      game.client_handle_mouse(mousePos)
+    }, false);
+
+
+		game.viewport.addEventListener('mousedown', function(evt) {
+		//var mousePos = getMousePos(game.viewport, evt);
+		//var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+		game.client_handle_mouseDown()
+	}, false);
+
+
+
 
 			//Set the draw style for the font
 		game.ctx.font = '11px "Helvetica"';
